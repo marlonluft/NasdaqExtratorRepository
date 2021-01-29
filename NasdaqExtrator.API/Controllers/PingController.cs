ac7bb5b5792d107c0b4ae5d2c5e067f3f969f26a
Marlon Luft Salvador <marlon.salvador@bludata.com.br>
2021-01-29 15:46:52 -0300

    Gravação de stock no BD

--- NasdaqExtrator.API/Controllers/PingController.cs
+++ NasdaqExtrator.API/Controllers/PingController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NasdaqExtrator.Core.Entity;
+using NasdaqExtrator.Core.Repository;
 using NasdaqExtrator.Core.Service;
 using System;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace NasdaqExtrator.API.Controllers
 {
@@ -10,10 +13,12 @@ namespace NasdaqExtrator.API.Controllers
     public class PingController : ControllerBase
     {
         private readonly INasdaqAPIService _nasdaqApiService;
+        private readonly IStockRepository _stockRepository;        
 
-        public PingController(INasdaqAPIService nasdaqApiService)
+        public PingController(INasdaqAPIService nasdaqApiService, IStockRepository stockRepository)
         {
             _nasdaqApiService = nasdaqApiService;
+            _stockRepository = stockRepository;
         }
 
         [HttpGet]
@@ -24,6 +29,22 @@ namespace NasdaqExtrator.API.Controllers
             var stockInfoTask = _nasdaqApiService.GetStockInfo("IBM");
 
             Task.WaitAll(dividendsTask, stockDividendsTask, stockInfoTask);
+
+            var stockResult = stockInfoTask.Result;
+            var stockDividendsResult = stockDividendsTask.Result;
+
+            var stock = new StockEntity(stockResult.Data.Symbol, stockResult.Data.CompanyName);
+
+            var id = _stockRepository.GravarStock(stock);
+
+            var dividendos = stockDividendsResult.Data.Dividends.Rows
+                .Select(x => new StockDividendEntity(decimal.Parse(x.Amount.Replace("$", string.Empty)), DateTime.Parse(x.PaymentDate)))
+                .ToList();
+
+            _stockRepository.AtualizarDividendos(id, dividendos);
+
+            //var preco = new StockPriceEntity(stockResult.)
+            //_stockRepository.AtualizarPrecos(id, dividendos);
 
             return Ok("Pong");
         }
