a0c29724efeaaa5a162a5ee43408ab55173ff60c
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2021-01-29 20:47:05 -0300

    Remoção de teste da controller ping

--- NasdaqExtrator.API/Controllers/PingController.cs
+++ NasdaqExtrator.API/Controllers/PingController.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NasdaqExtrator.Core.Entity;
-using NasdaqExtrator.Core.Repository;
-using NasdaqExtrator.Core.Service;
-using System;
-using System.Threading.Tasks;
-using System.Linq;
 
 namespace NasdaqExtrator.API.Controllers
 {
@@ -12,39 +6,9 @@ namespace NasdaqExtrator.API.Controllers
     [Route("[controller]")]
     public class PingController : ControllerBase
     {
-        private readonly INasdaqAPIService _nasdaqApiService;
-        private readonly IStockRepository _stockRepository;        
-
-        public PingController(INasdaqAPIService nasdaqApiService, IStockRepository stockRepository)
-        {
-            _nasdaqApiService = nasdaqApiService;
-            _stockRepository = stockRepository;
-        }
-
         [HttpGet]
         public IActionResult Get()
         {
-            var stockDividendsTask = _nasdaqApiService.GetStockDividends("IBM");
-            var stockInfoTask = _nasdaqApiService.GetStockInfo("IBM");
-
-            Task.WaitAll(stockDividendsTask, stockInfoTask);
-
-            var stockResult = stockInfoTask.Result;
-            var stockDividendsResult = stockDividendsTask.Result;
-
-            var stock = new StockEntity(stockResult.Data.Symbol, stockResult.Data.CompanyName);
-
-            var id = _stockRepository.GravarStock(stock);
-
-            var dividendos = stockDividendsResult.Data.Dividends.Rows
-                .Select(x => new StockDividendEntity(decimal.Parse(x.Amount.Replace("$", string.Empty)), DateTime.Parse(x.PaymentDate)))
-                .ToList();
-
-            _stockRepository.AtualizarDividendos(id, dividendos);
-
-            //var preco = new StockPriceEntity(stockResult.)
-            //_stockRepository.AtualizarPrecos(id, dividendos);
-
             return Ok("Pong");
         }
     }
