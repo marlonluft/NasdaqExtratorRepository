1c2be0540820f568c2aa1581dc8e60f376dbf2b7
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2021-01-29 22:05:33 -0300

    Adicionado hangfire para importação de histórico de dividendos do dia anterior

--- NasdaqExtrator.API/Startup.cs
+++ NasdaqExtrator.API/Startup.cs
@@ -1,17 +1,27 @@
+using Hangfire;
+using Hangfire.Mongo;
+using Hangfire.Mongo.Migration.Strategies;
+using Hangfire.Mongo.Migration.Strategies.Backup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
 using NasdaqExtrator.API.Util;
+using NasdaqExtrator.Core.Service;
+using System;
 
 namespace NasdaqExtrator.API
 {
     public class Startup
     {
+        private readonly string _mongoDB;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _mongoDB = Configuration["MongoDbSettings:ConnectionString"];
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +34,8 @@ namespace NasdaqExtrator.API
             services.ConfigurarAppSettings(Configuration);
             services.ConfigurarHttpClient(Configuration);
             services.RegistrarInjecaoDependencia();
+
+            ConfigurarHangFire(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,10 +52,43 @@ namespace NasdaqExtrator.API
 
             app.UseAuthorization();
 
+            app.UseHangfireDashboard();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            RegistrarJobsRecorrentes();
+        }
+
+        private void ConfigurarHangFire(IServiceCollection services)
+        {
+            var mongoUrlBuilder = new MongoUrlBuilder(_mongoDB);
+            var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+
+            // Add Hangfire services. Hangfire.AspNetCore nuget required
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseMongoStorage(mongoClient, mongoUrlBuilder.DatabaseName, new MongoStorageOptions
+                {
+                    MigrationOptions = new MongoMigrationOptions
+                    {
+                        MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                        BackupStrategy = new CollectionMongoBackupStrategy()
+                    },
+                    Prefix = "hangfire.NasdaqExtrator",
+                    CheckConnection = true
+                })
+            );
+            services.AddHangfireServer();
+        }
+
+        private void RegistrarJobsRecorrentes()
+        {
+            RecurringJob.AddOrUpdate<IDividendHistoryService>("ImportarHistorico", x => x.ImportarHistorico(DateTime.Now.AddDays(-1)), Cron.Daily);
         }
     }
 }
