eddb7399d9a502f6fe7c0560599c993f92957509
Marlon Luft Salvador <marlon.salvador@bludata.com.br>
2021-02-01 14:42:13 -0300

    Tela inicial de exemplo

--- NasdaqExtrator.API/wwwroot/nasdaq-extrator-web/src/Views/TelaInicialView.js
+++ NasdaqExtrator.API/wwwroot/nasdaq-extrator-web/src/Views/TelaInicialView.js
@@ -5,6 +5,103 @@ class TelaInicialView extends Component {
         return (
             <div>
                 <h2>Bem vindo</h2>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan={4}>Top 10 Dividendos Pagos 2020</th>
+                        </tr>
+                        <tr>
+                            <th>Simbolo</th>
+                            <th>Valor Médio Pago</th>
+                            <th>Valor Total Pago</th>
+                            <th>Qtd Dividendos Pagos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>IBM</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.60</td>
+                            <td>6</td>
+                        </tr>
+                        <tr>
+                            <td>ERP</td>
+                            <td>$ 1.0</td>
+                            <td>$ 0.10</td>
+                            <td>10</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan={4}>Top 10 Pagadoras Dividendos Estáveis</th>
+                        </tr>
+                        <tr>
+                            <th>Simbolo</th>
+                            <th>Valor Médio Pago</th>
+                            <th>V.M. 2020</th>
+                            <th>V.M. 2019</th>
+                            <th>V.M. 2018</th>
+                            <th>V.M. 2017</th>
+                            <th>Qtd Média Dividendos Pagos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>IBM</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>6</td>
+                        </tr>
+                        <tr>
+                            <td>ERP</td>
+                            <td>$ 1.0</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>$ 0.10</td>
+                            <td>10</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan={4}>Top 10 Stock Crescentes</th>
+                        </tr>
+                        <tr>
+                            <th>Simbolo</th>
+                            <th>Cresc. 2020</th>
+                            <th>Cresc. 2019</th>
+                            <th>Cresc. 2018</th>
+                            <th>Cresc. 2017</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>IBM</td>
+                            <td>10%</td>
+                            <td>10%</td>
+                            <td>10%</td>
+                            <td>10%</td>
+                        </tr>
+                        <tr>
+                            <td>ERP</td>
+                            <td>9%</td>
+                            <td>10%</td>
+                            <td>10%</td>
+                            <td>10%</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </div>
         )
     }
