be062883810f47df9c61e5d61911c723c7bb1319
Marlon Luft Salvador <marlon.salvador@bludata.com.br>
2021-02-01 13:31:34 -0300

    Criação de entidade de dados consolidados para exibição em tela

---
+++ NasdaqExtrator.Core/Entity/Consolidado/EvolucaoDividendosAnoEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace NasdaqExtrator.Core.Entity.Consolidado
+{
+    public class EvolucaoDividendosAnoEntity
+    {
+        public EvolucaoDividendosAnoEntity(int ano, decimal valorMedioDividendo)
+        {
+            Ano = ano;
+            ValorMedioDividendo = valorMedioDividendo;
+        }
+
+        public int Ano { get; set; }
+        public decimal ValorMedioDividendo { get; set; }
+    }
+}
---
+++ NasdaqExtrator.Core/Entity/Consolidado/EvolucaoDividendosEntity.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace NasdaqExtrator.Core.Entity.Consolidado
+{
+    public class EvolucaoDividendosEntity
+    {
+        public EvolucaoDividendosEntity(string simbolo, int quantidadeMediaDividendosPagos, List<EvolucaoDividendosAnoEntity> anos)
+        {
+            Simbolo = simbolo;
+            QuantidadeMediaDividendosPagos = quantidadeMediaDividendosPagos;
+            Anos = anos;
+        }
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Simbolo { get; set; }
+        public List<EvolucaoDividendosAnoEntity> Anos { get; set; }
+        public int QuantidadeMediaDividendosPagos { get; set; }
+    }
+}
